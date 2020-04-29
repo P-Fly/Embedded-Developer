5096369fa091d2ab3f9ffceea3812d07e62cc939
P-Fly <27144363@qq.com>
2020-04-30 06:47:46 +0800

    Add stm32_assert.h to hal layer

--- embedded_device_sw/products/stm32wb55_demo/conf/FreeRTOSConfig.h
+++ embedded_device_sw/products/stm32wb55_demo/conf/FreeRTOSConfig.h
@@ -132,7 +132,7 @@ standard names. */
 
 /* IMPORTANT: This define is commented when used with STM32Cube firmware, when the timebase source is SysTick,
               to prevent overwriting SysTick_Handler defined within STM32Cube HAL */
-#define xPortSysTickHandler SysTick_Handler
+#define osSystickHandler SysTick_Handler
 
 /* USER CODE BEGIN Defines */   	      
 /* Section where parameter definitions can be added (for instance, to override default ones in FreeRTOS.h) */
--- embedded_device_sw/products/stm32wb55_demo/conf/stm32wbxx_hal_conf.h
+++ embedded_device_sw/products/stm32wb55_demo/conf/stm32wbxx_hal_conf.h
@@ -334,9 +334,8 @@
   *         If expr is true, it returns no value.
   * @retval None
   */
-  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((char *)__FILE__, __LINE__))
-/* Exported functions ------------------------------------------------------- */
-  void assert_failed(char* file, uint32_t line);
+#include <assert.h>
+  #define assert_param(expr) assert(expr);
 #else
   #define assert_param(expr) ((void)0U)
 #endif /* USE_FULL_ASSERT */
